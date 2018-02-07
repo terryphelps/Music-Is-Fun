cb8181bd37488b1cfe2ecc31e9c02a826e61f1fc
Mark R Ohnsman <MarkOhnsman@users.noreply.github.com>
2018-02-07 11:41:07 -0700

    removes bcw getter

--- itunes-service.js
+++ itunes-service.js
@@ -3,15 +3,13 @@ function ItunesService(){
     this.getMusicByArtist = function(artist) {
 
       //allows requests to localhost: 8080 otherwise blocked by itunes
-      var url = '//bcw-getter.herokuapp.com/?url=';
-      var url2 = 'https://itunes.apple.com/search?term=' + artist;
-      var apiUrl = url + encodeURIComponent(url2);
+      var url = 'https://itunes.apple.com/search?term=' + artist;
       
       //changes the button to loading while songs load
       $('#get-music-button').text('LOADING....');
       
       //modifies the objects to reduce the excess data
-      return $.getJSON(apiUrl).then(function(response){
+      return $.getJSON(url).then(function(response){
         var songList = response.results.map(function (song) {
                   return {
                       title: song.trackName,
