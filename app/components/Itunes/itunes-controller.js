16bce7d6078fd864390697751e806c70bd0a3d15
terryphelps <terry.r.phelps@gmail.com>
2019-06-21 16:46:00 -0600

    Working

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -6,13 +6,20 @@ function drawSongs() {
   //changes button back to GET MUSIC once songs are loaded
   document.querySelector('#get-music-button').textContent = 'GET MUSIC'
   console.log(itunesService.Songs)
-
+  let songsElem = document.querySelector("#songs")
+  let template = ""
+  let itunes = itunesService.Songs
+  itunes.forEach(itunes => {
+    template += itunes.Template
+  })
+  songsElem.innerHTML = template
 }
 
 
 //PUBLIC
 class ItunesController {
   constructor() {
+    itunesService.addSubscriber("songs", drawSongs)
     //BE SURE TO REGISTER YOUR SUBSCRIBERS!!!!!!!
   }
 
@@ -29,3 +36,4 @@ class ItunesController {
 
 
 export default ItunesController
+drawSongs()
--- app/models/Song.js
+++ app/models/Song.js
@@ -2,10 +2,24 @@ export default class Song {
     constructor(song) {
         this.title = song.trackName
         //Change 250x250 if you want a larger image
-        this.albumArt = song.artworkUrl60.replace(/60x60/g, "250x250")
+        this.albumArt = song.artworkUrl60.replace(/60x60/g, "150x150")
         this.artist = song.artistName
         this.collection = song.collectionName
         this.price = song.collectionPrice
         this.preview = song.previewUrl
+    }
+
+
+    get Template() {
+        return `
+        <div class="col-4">
+            <h2>Artist:${this.artist}</h2>
+            <h3>Title:${this.title}</h3>
+            <h3>Collection:${this.collection}</h3>
+            <img src="${this.albumArt}" alt="Album Art">
+            <h3>Price:${this.price}</h3>
+            <audio controls><source src="${this.preview}"></audio>
+        </div>
+    `
     }
 }
